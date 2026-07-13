189d318e5785310a218eab08ef735c9512a3e82b
Jacob Glide <glidej@Jacobs-MacBook-Neo.local>
2026-07-13 12:47:12 -0400

    refactor: consume common vehicle data

--- src/app/core/services/bid-list.service.ts
+++ src/app/core/services/bid-list.service.ts
@@ -1,9 +1,8 @@
 import { Injectable, inject } from '@angular/core';
+import { buildBidList, MOCK_BID_RECORDS } from '@drivewise/common-data';
 import { map, Observable } from 'rxjs';
 
-import { MOCK_BID_RECORDS } from '../data/mock-bids';
-import { VehicleBidListItem, VehicleBidRecord } from '../models/bid';
-import { Vehicle } from '../models/vehicle';
+import { VehicleBidListItem } from '../models/bid';
 import { InventoryService } from './inventory.service';
 
 @Injectable({ providedIn: 'root' })
@@ -11,29 +10,8 @@ export class BidListService {
   private readonly inventoryService = inject(InventoryService);
 
   getBidList(): Observable<VehicleBidListItem[]> {
-    return this.inventoryService.getVehicles().pipe(
-      map((vehicles) => {
-        const vehiclesById = new Map(vehicles.map((vehicle) => [vehicle.id, vehicle]));
-
-        return MOCK_BID_RECORDS.flatMap((bid) => {
-          const vehicle = vehiclesById.get(bid.vehicleId);
-
-          return vehicle ? [this.toBidListItem(bid, vehicle)] : [];
-        });
-      }),
-    );
-  }
-
-  private toBidListItem(bid: VehicleBidRecord, vehicle: Vehicle): VehicleBidListItem {
-    return {
-      ...bid,
-      vehicle: {
-        ...vehicle,
-        features: [...vehicle.features],
-        history: { ...vehicle.history },
-        location: { ...vehicle.location },
-        tags: [...vehicle.tags],
-      },
-    };
+    return this.inventoryService
+      .getVehicles()
+      .pipe(map((vehicles) => buildBidList(MOCK_BID_RECORDS, vehicles)));
   }
 }
